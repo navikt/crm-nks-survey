23ea6d467a9af5784c35c03fd64a8a5b2a9fbe99
saramoh2309 <86772218+saramoh2309@users.noreply.github.com>
2022-10-25 12:25:40 +0200

    removed apex class + changed survey component

--- force-app/main/default/lwc/nksSurveyMain/nksSurveyMain.js
+++ force-app/main/default/lwc/nksSurveyMain/nksSurveyMain.js
@@ -1,32 +1,19 @@
-import { LightningElement, track } from 'lwc';
-import createSurveyInvitation from '@salesforce/apex/NKS_SurveyController.createSurveyInvitation';
+import { LightningElement, track, api } from 'lwc';
 import newsIcon from '@salesforce/resourceUrl/newsIcon';
 import homeIcon from '@salesforce/resourceUrl/homeIcon';
 
 export default class Nks_SurveyCommunity extends LightningElement {
     @track error;
     @track result;
+    @api url;
     news = newsIcon;
     home = homeIcon;
 
     openSurvey() {
-        createSurveyInvitation()
-            .then((returnedResult) => {
-                this.result = returnedResult;
-                let obj = JSON.parse(JSON.stringify(this.result));
-
-                if (obj.error === true) {
-                    console.log(obj.res);
-                }
-                if (obj.error === false) {
-                    console.log(obj.res);
-                    window.open(obj.res);
-                }
-                this.error = undefined;
-            })
-            .catch((error) => {
-                this.result = error;
-                this.error = undefined;
-            });
+        try {
+            window.open(this.url);
+        } catch (error) {
+            console.log(error);
+        }
     }
 }
