a895f9651c4ecd2e22e9a52b3d5ce9399b35b3ba
saramoh2309 <86772218+saramoh2309@users.noreply.github.com>
2022-10-25 12:27:56 +0200

    remoed unused variables

--- force-app/main/default/lwc/nksSurveyMain/nksSurveyMain.js
+++ force-app/main/default/lwc/nksSurveyMain/nksSurveyMain.js
@@ -1,10 +1,8 @@
-import { LightningElement, track, api } from 'lwc';
+import { LightningElement, api } from 'lwc';
 import newsIcon from '@salesforce/resourceUrl/newsIcon';
 import homeIcon from '@salesforce/resourceUrl/homeIcon';
 
 export default class Nks_SurveyCommunity extends LightningElement {
-    @track error;
-    @track result;
     @api url;
     news = newsIcon;
     home = homeIcon;
