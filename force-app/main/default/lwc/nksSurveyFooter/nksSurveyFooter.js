8e7a5da6750890579e9ab682b7df81301cc634d1
saramoh2309 <86772218+saramoh2309@users.noreply.github.com>
2023-03-16 09:11:43 +0100

    removed unused files

--- force-app/main/default/lwc/nksSurveyFooter/nksSurveyFooter.js
+++
@@ -1,6 +0,0 @@
-import { LightningElement } from 'lwc';
-import resource from '@salesforce/resourceUrl/navLogoWhite';
-
-export default class NksSurveyFooter extends LightningElement {
-    logo = resource;
-}
